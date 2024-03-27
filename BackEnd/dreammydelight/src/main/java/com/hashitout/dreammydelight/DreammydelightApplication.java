edfd7e5c21079b7a4d68b138ed6efa4b89db0efe
deshini <sharuhari1525@gmail.com>
2024-03-27 15:55:44 +0530

    backend

---
+++ BackEnd/dreammydelight/src/main/java/com/hashitout/dreammydelight/DreammydelightApplication.java
@@ -0,0 +1,13 @@
+package com.hashitout.dreammydelight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreammydelightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreammydelightApplication.class, args);
+	}
+
+}
---
+++ BackEnd/dreammydelight/src/test/java/com/hashitout/dreammydelight/DreammydelightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashitout.dreammydelight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreammydelightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
